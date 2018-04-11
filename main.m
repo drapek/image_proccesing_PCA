b07158ac906fc4463d3625470d19a3241e808bfe
Paweł Drapiewski <drapek39@gmail.com>
2018-04-11 13:20:09 +0200

    Image reading module

---
+++ main.m
@@ -0,0 +1,9 @@
+% PCA - Principal Component Analysis
+% Authors: Dariusz Dybka, Pawe³ Drapiewski
+
+%% Main file - start point for the algorithm
+
+% Read the images
+read_images
+
+% TODO process them
